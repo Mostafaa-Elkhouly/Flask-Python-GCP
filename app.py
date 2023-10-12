55a9af8dcd4cf0acf62f1ad14f864033974ee446
Mostafaa-Elkhouly <ms.elkhouly97@gmail.com>
2023-10-12 15:18:56 +0300

    Edit app.py

--- app.py
+++ app.py
@@ -4,5 +4,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
+def index():
     return '<h1>Hello From GKE, Mostafa Elkhouly ^_^</h1>'
+
+# main driver function
+if __name__ == "__main__":
+    app.run()
